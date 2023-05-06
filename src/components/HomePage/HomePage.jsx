e8e825a4b4829cbec6026899eec665409fb651ff
Luong Quang Huy <luongquanghuy@MacBook-Pro-cua-Luong.local>
2023-05-06 16:22:25 +0700

    Footer

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Col, Row, Typography } from 'antd';
-import styles from './Homepage.module.css';
-import Logo from '../Logo/Logo';
-import backgroundLandingPage from '../../../public/landing-page/background-landing-page.jpg'
+import React from "react";
+import { Col, Row, Typography } from "antd";
+import styles from "./Homepage.module.css";
+import Logo from "../Logo/Logo";
 
 function HomePage() {
-
-  const { Title } = Typography
+  const { Title } = Typography;
   return (
-    <div className={styles.container} style={{backgroundImage: `url(${backgroundLandingPage})`}}>
-        <Row justify="center">
-          <Col className={styles.textContainer} span={18}>
-            <Row justify='center'>
-              <Col span={6}>
-                <Logo />
-              </Col>
-            </Row>
-            <Title className={styles.text}>ONLINE ORDER</Title>
-            <Title className={styles.text} level={3}>How do you want to recceive your order ?</Title>
-          </Col>
-        </Row>
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: "url(/landing-page/background-landing-page.png",
+      }}
+    >
+      <Row justify="center">
+        <Col className={styles.textContainer} span={18}>
+          <Row justify="center">
+            <Col span={6}>
+              <Logo />
+            </Col>
+          </Row>
+          <Title className={styles.text}>ONLINE ORDER</Title>
+          <Title className={styles.text} level={3}>
+            How do you want to recceive your order ?
+          </Title>
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
