e47b4c1947f31604212a021de7bf3f473beeaf59
Luong Quang Huy <luongquanghuy@MacBook-Pro-cua-Luong.local>
2023-05-11 19:50:04 +0700

    Add receive option for customer

--- src/pages/delivery/index.jsx
+++ src/pages/delivery/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Typography, Row, Col } from "antd";
+import { Typography, Row, Col, Radio } from "antd";
+import { useState } from "react";
 import styles from "./Delivery.module.css";
 
 const { Title, Text } = Typography;
 
 export default function Delivery() {
+  //Set value for radio
+  const [value, setValue] = useState(1);
+
+  const onChange = (e) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <Row justify="center">
@@ -13,6 +22,10 @@ export default function Delivery() {
             Delivery
           </Title>
           <Title level={5}>When would you like to receive your order?</Title>
+          <Radio.Group onChange={onChange} value={value}>
+            <Radio value={1}>As soon as possible</Radio>
+            <Radio value={2}>Later</Radio>
+          </Radio.Group>
         </Col>
       </Row>
     </div>
