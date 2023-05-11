8592aad502b39d39a077b114c03b8f2ae304e5d9
Luong Quang Huy <luongquanghuy@MacBook-Pro-cua-Luong.local>
2023-05-11 19:40:47 +0700

    add text to delivery page

--- src/pages/delivery/index.jsx
+++ src/pages/delivery/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Typography } from "antd";
+import { Typography, Row, Col } from "antd";
 import styles from "./Delivery.module.css";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export default function Delivery() {
   return (
-    <div>
-      <Title className={styles.HeaderPage}>Delivery</Title>
+    <div className={styles.container}>
+      <Row justify="center">
+        <Col sx={20} md={12} lg={8} xl={8}>
+          <Title level={2} className={styles.pageHeader}>
+            Delivery
+          </Title>
+          <Title level={5}>When would you like to receive your order?</Title>
+        </Col>
+      </Row>
     </div>
   );
 }
